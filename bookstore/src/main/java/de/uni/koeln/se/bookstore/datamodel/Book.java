108adbd9d15feaa81fbfe003be38f5bd064e4a5d
Marc Peter <mpeter6@smail.uni-koeln.de>
2023-01-15 19:38:58 +0100

    first commit

---
+++ bookstore/src/main/java/de/uni/koeln/se/bookstore/datamodel/Book.java
@@ -0,0 +1,83 @@
+package de.uni.koeln.se.bookstore.datamodel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+    private String author;
+    private Integer year;
+
+    public Book() {
+    }
+
+    public Book(String name, String author, Integer year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return Integer return the year
+     */
+    public Integer getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+}
